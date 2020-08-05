e17af90e1077b907737abca70523d865981d33f5
Matthew Janaszczyk <matthew.janaszczyk@GL08141M.local>
2020-08-05 13:08:36 +0200

    feat: started on querying mutations

--- api/src/schema.js
+++ api/src/schema.js
@@ -4,8 +4,13 @@ const { gql } = require('apollo-server')
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
-
+{
+    characters {
+        results {
+            name
+        }
+    }
+}
 `;
 
 module.exports = typeDefs
--- client/src/client.js
+++ client/src/client.js
@@ -3,7 +3,29 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
 import gql from 'graphql-tag'
 
+const link = new HttpLink({uri: 'https://rickandmortyapi.com/graphql'})
 
-const client = new ApolloClient()
+const cache = new InMemoryCache()
+
+const client = new ApolloClient({
+    link,
+    cache
+})
+
+// const query = gql`
+//     {
+//         characters {
+//             results {
+//                 id
+//                 name
+//             }
+//         }
+//     }
+// `
+
+client.query({query})
+    .then(result => console.log(result))
 
 export default client
+
+// TODO integrate API
